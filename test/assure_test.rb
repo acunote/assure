9d1f144f013dd1cd3780f890bfbda0b9bc288af8
Alexander Dymo <adymo@pluron.com>
2013-12-26 16:44:24 -0600

    Revert "Update tests to pass in all new ruby versions. 1.8 does not have to be supported at this time." This reverts commit 836025a3a1b6ffdb75ec516aa965998f0df47651.

--- test/assure_test.rb
+++ test/assure_test.rb
@@ -11,23 +11,23 @@ class AssureTest < Test::Unit::TestCase
         exception = assert_raise(RuntimeError) do
             assure(false)
         end
-        assert_equal '/test/assure_test.rb:12: block in test_basic: Assertion "false" failed', exception.message
+        assert_equal 'test/assure_test.rb:12: test_basic: Assertion "false" failed', exception.message
 
         exception = assert_raise(RuntimeError) do
             assure(nil)
         end
-        assert_equal '/test/assure_test.rb:17: block in test_basic: Assertion "nil" failed', exception.message
+        assert_equal 'test/assure_test.rb:17: test_basic: Assertion "nil" failed', exception.message
 
         exception = assert_raise(RuntimeError) do
             x = 2
             assure( x == 3, "#{x} == 2" )
         end
-        assert_equal "/test/assure_test.rb:23: block in test_basic: Assertion \"x == 3, \"\#{x} == 2\"\" failed\n2 == 2", exception.message
+        assert_equal "test/assure_test.rb:23: test_basic: Assertion \"x == 3, \"\#{x} == 2\"\" failed\n2 == 2", exception.message
 
         exception = assert_raise(RuntimeError) do
             assure(1.is_a? String)
         end
-        assert_equal '/test/assure_test.rb:28: block in test_basic: Assertion "1.is_a? String" failed', exception.message
+        assert_equal 'test/assure_test.rb:28: test_basic: Assertion "1.is_a? String" failed', exception.message
     end
 
 end
